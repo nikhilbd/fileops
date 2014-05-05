4b5072cc5c99a556d36eee962bd7e94aea1e489a
Nikhil <nikhil@foursquare.com>
2014-05-05 23:52:48 +0000

    Turn into a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='fileops',
+    version='0.10',
+    description='Python scripts for useful file operations',
+    author='Nikhil Dandekar',
+    author_email='nikhildandekar@gmail.com',
+    url='http://github.com/nikhilbd/command-line-scripts',
+    packages=['fileops']
+)
